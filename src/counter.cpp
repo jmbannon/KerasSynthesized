62a13d681af5a98346c56fdc1913185d04d76dd2
Jesse Bannon <jmbannon@uw.edu>
2018-06-15 09:25:31 -0700

    Modify makefile for parameter convolution size. Print out in beginning of test

--- src/counter.cpp
+++ src/counter.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
 #include "test/test_component_convolver.hpp"
 
+using namespace std;
+
 int main() {
+  stringstream ss;
+  ss << CONVOLVER_TEST_INPUT_SIZE;
+  string conv_input_size = ss.str();
+  const string test_name = string("test_component_3_3_convolver_") + conv_input_size + string("_") + conv_input_size;
+  cout << test_name << endl;
+
   int ret = test_component_3_3_convolver_variable();
 
   if (ret == 0) {
