efb41a2bb6faf391f8bc14f4ddd0391d3ac749f8
Jesse Bannon <jmbannon@uw.edu>
2018-09-25 21:33:18 -0700

    Add more funcs to tiled_tensor3

--- src/tiled_tensor3.hpp
+++ src/tiled_tensor3.hpp
@@ -67,7 +67,7 @@ int tiled_tensor3_init(tiled_tensor3 *tensor, uint rows, uint cols, uint depth, 
   return 0;
 }
 
-int tensor3_init_padding(tiled_tensor3 *tensor, uint rows, uint cols, uint depth, uint tile_rows, uint tile_cols, uint tile_depth, Major maj_t, Major tile_maj, uint paddingY, uint paddingX) {
+int tiled_tensor3_init_padding(tiled_tensor3 *tensor, uint rows, uint cols, uint depth, uint tile_rows, uint tile_cols, uint tile_depth, Major maj_t, Major tile_maj, uint paddingY, uint paddingX) {
   return tiled_tensor3_init(tensor, rows + (2 * paddingY), cols + (2 * paddingX), depth, tile_rows, tile_cols, tile_depth, maj_t, tile_maj);
 }
 
@@ -140,6 +140,17 @@ void tiled_tensor3_set_val(tiled_tensor3 *t, uint row, uint col, uint dep, Numer
   t->data[tiled_tensor3_idx(t, row, col, dep)] = val;
 }
 
+int tiled_tensor3_fill_val(tiled_tensor3 *t, Numeric val) {
+  for (uint i = 0; i < t->depth; i++) {
+    for (uint j = 0; j < t->rows; j++) {
+      for (uint k = 0; k < t->cols; k++) {
+        tiled_tensor3_set_val(t, j, k, i, val);
+      }
+    }
+  }
+  return 0;
+}
+
 int tiled_tensor3_set_data_sequential_raw(tiled_tensor3 *t, bool row, int paddingY, int paddingX) {
   for (uint i = 0; i < t->depth; i++) {
     for (uint j = 0; j < t->rows; j++) {
@@ -154,5 +165,27 @@ int tiled_tensor3_set_data_sequential_raw(tiled_tensor3 *t, bool row, int paddin
   }
   return 0;
 }
+
+int tiled_tensor3_fill_zero(tiled_tensor3 *t) {
+  return tiled_tensor3_fill_val(t, 0.0);
+}
+
+int tiled_tensor3_set_data_sequential_row(tiled_tensor3 *t) {
+  return tiled_tensor3_set_data_sequential_raw(t, true, 0, 0);
+}
+
+int tiled_tensor3_set_data_sequential_row_padding(tiled_tensor3 *t, int paddingY, int paddingX) {
+  return tiled_tensor3_set_data_sequential_raw(t, true, paddingY, paddingX);
+}
+
+int tiled_tensor3_set_data_sequential_col(tiled_tensor3 *t) {
+  return tiled_tensor3_set_data_sequential_raw(t, false, 0, 0);
+}
+
+int tiled_tensor3_set_data_sequential_col_padding(tiled_tensor3 *t, int paddingY, int paddingX) {
+  return tiled_tensor3_set_data_sequential_raw(t, false, paddingY, paddingX);
+}
+
+
 
 #endif
